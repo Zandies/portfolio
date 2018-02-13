438553dfe2443038f055849d8d22572cfd9ca36c
Zandies Scepoli <zandies.2017@gmail.com>
2018-02-13 23:25:56 +0100

    MàJ JS

--- test.js
+++ test.js
@@ -1,36 +1,36 @@
 $(function(){
-	$HelloWorld = $("#caption p");
-	$cookie = $("#ulysse");
-	if ($(document).ready()){
-		$HelloWorld.addClass('animated zoomIn');
-	}
+		$HelloWorld = $("#caption p");
+		$cookie = $("#ulysse");
+		if ($(document).ready()){
+			$HelloWorld.addClass('animated zoomIn');
+		}
 
-	$HelloWorld.on('click', function(){
-		$HelloWorld.removeClass('zoomIn').addClass('animated rubberBand');
-	});
+		$HelloWorld.on('click', function(){
+			$HelloWorld.removeClass('zoomIn').addClass('animated rubberBand');
+		});
 
-	// $cookie.on('mouseover', function(){
-	// 	$cookie.addClass('animated infinite slideInLeft');
-	// 	// $cookie.removeClass('animated bounce');
-	// });
+		// $cookie.on('mouseover', function(){
+		// 	$cookie.addClass('animated infinite slideInLeft');
+		// 	// $cookie.removeClass('animated bounce');
+		// });
 
-	// $cookie.on('mouseout', function(){
-	// 	// $cookie.addClass('animated infinite bounce');
-	// 	$cookie.removeClass('animated infinte slideInLeft');
-	// });
+		// $cookie.on('mouseout', function(){
+		// 	// $cookie.addClass('animated infinite bounce');
+		// 	$cookie.removeClass('animated infinte slideInLeft');
+		// });
 
-	// $cookie.on('click', function(){
-	// 	$cookie.html(' &#128165; ').removeClass('animated infinte bounce');
-	// });
+		// $cookie.on('click', function(){
+		// 	$cookie.html(' &#128165; ').removeClass('animated infinte bounce');
+		// });
 
-    // Lorsqu'on clique sur "Contact" -> Scroll Smooth vers l'ancre
-    $('#contact').on('click', function(e){
-	       e.preventDefault(); 
-		var target = $(this).attr('href');
-		$('html, body').animate({
-			scrollTop: $(target).offset().top
-		}, 1000 );
-	});
+	    // Lorsqu'on clique sur "Contact" -> Scroll Smooth vers l'ancre
+	    $('#contact').on('click', function(e){
+		       e.preventDefault(); 
+			var target = $(this).attr('href');
+			$('html, body').animate({
+				scrollTop: $(target).offset().top
+			}, 1000 );
+		});
 
 });
 
