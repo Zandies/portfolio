df17666b752b3affc31e1240e40d257dad8cce3c
Zandies Scepoli <zandies.2017@gmail.com>
2018-02-13 02:10:42 +0100

    Add Code controlled and burger button (but it does'nt work ^^')

--- test.js
+++ test.js
@@ -33,3 +33,28 @@ $(function(){
 	});
 
 });
+
+(function(){
+
+	var scrollY = function () {
+		var supportPageOffset = window.pageXOffset !== undefined;
+		var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+		return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+	};
+
+	var element = document.querySelector('header')
+	var top = element.getBoundingClientRect().top + scrollY()
+	var onScroll = function () {
+		if (scrollY() > top) {
+			element.classList.add('fixed')
+		}else{
+			element.classList.remove('fixed')
+		}
+	};
+	window.addEventListener('scroll', onScroll);
+
+})()
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
