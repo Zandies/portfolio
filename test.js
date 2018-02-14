c54252a2cb8c460e5a238b1af2d9f0668eaf29f5
Zandies Scepoli <zandies.2017@gmail.com>
2018-02-14 13:11:40 +0100

    Reduction of animation css file's size

--- test.js
+++ test.js
@@ -1,27 +1,13 @@
 $(function(){
 		$HelloWorld = $("#caption p");
-		$cookie = $("#ulysse");
+		
 		if ($(document).ready()){
-			$HelloWorld.addClass('animated zoomIn');
+			$HelloWorld.addClass('animé zoom');
 		}
 
 		$HelloWorld.on('click', function(){
-			$HelloWorld.removeClass('zoomIn').addClass('animated rubberBand');
+			$HelloWorld.removeClass('zoom').addClass('animé elastique');
 		});
-
-		// $cookie.on('mouseover', function(){
-		// 	$cookie.addClass('animated infinite slideInLeft');
-		// 	// $cookie.removeClass('animated bounce');
-		// });
-
-		// $cookie.on('mouseout', function(){
-		// 	// $cookie.addClass('animated infinite bounce');
-		// 	$cookie.removeClass('animated infinte slideInLeft');
-		// });
-
-		// $cookie.on('click', function(){
-		// 	$cookie.html(' &#128165; ').removeClass('animated infinte bounce');
-		// });
 
 	    // Lorsqu'on clique sur "Contact" -> Scroll Smooth vers l'ancre
 	    $('#contact').on('click', function(e){
@@ -55,6 +41,6 @@ $(function(){
 
 })()
 
-function myFunction(x) {
+function burger(x) {
     x.classList.toggle("change");
 }
