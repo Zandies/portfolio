5d1ab86ee4b67fec88124c9cf073723472b1e838
Zandies Scepoli <zandies.2017@gmail.com>
2018-02-17 00:11:26 +0100

    Add responsive effect and author div

--- php/blog.php
+++ php/blog.php
@@ -5,14 +5,14 @@
     <script src="js/jquery.js"></script>
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
     <script type="text/javascript">
-    window.addEventListener('load', function(){
-      document.getElementById('loader').style.display = 'none';
-    });
+	    window.addEventListener('load', function(){
+	      document.getElementById('loader').style.display = 'none';
+	    });
     </script>
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
     <link rel="stylesheet" href="../css/article.css">
     <link rel="stylesheet" href="../css/responsive.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
+    <!-- <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">  -->
     <link rel="stylesheet" href="../css/animations.css">
     <!-- <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script> -->
     <link rel="icon" href="../img/favicon.ico" />
@@ -36,7 +36,7 @@
      <a href="#" class="subnav-option-burger">PHP/MySQl</a>
      <a href="#" class="subnav-option-burger">Linux</a>
      <div class="dropdown">
-        <a class="nav-option" href="#" id="dropbtn">Code</a>
+        <a class="nav-option" id="dropbtn">Code</a>
         <div class="dropdown-content">
           <a class="subnav-option" href="#">HTML/CSS</a>
           <a class="subnav-option" href="#">Javascript/JQuery</a>
@@ -45,36 +45,39 @@
         </div>
     </div>
      <a class="nav-option" href="#box3" id="contact">Contact</a>
+ 	</nav>
   </header>
  
+  <div class="banner-article">
+  	<p id="banner-title">Lorem ipsum</p>
+  </div>
+  
   <article>
-  	<div class="banner-article">
-  		<p id="banner-title">Lorem ipsum</p>
-  	</div>
+	<aside class="info-article">
+		<img id="author" src="../img/author.jpeg">
+		<p>Zandies Scepoli</p>
+		<p>16 février 2018</p>
+	</aside>
   	<div class="content-article">
-	  	<!-- <div class="info-article">
-	  		<img id="author" src="../img/author.jpg">
-	  	</div> -->
   		<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident nobis nostrum nam, error et labore. Atque, molestiae.</h3>
   		<p>Salut !  Ceci est mon premier article "intitulé "Lorem ipsum", un nom bien étrange qui est utilisé dans le domaine de l'informatique pour remplir des zones qui contiendront du texte mais on ne sait pas encore lequel.
-  		Donc voici le texte : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, aspernatur. Cupiditate dolor quae debitis, sequi saepe sit deserunt nobis porro, magnam, placeat omnis, reprehenderit vel quam voluptates soluta fuga magni." Cela signifie un truc comme "Personne n'aime la douleur pour la douleur...". Ha et oui, Hufflepuff Forever ! ^^</p>
-  		<img id="hufflepuff" src="../img/Hufflepuff_Logo.png">
+  		Donc voici le texte : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, aspernatur. Cupiditate dolor quae debitis, sequi saepe sit deserunt nobis porro, magnam, placeat omnis, reprehenderit vel quam voluptates soluta fuga magni." Cela signifie un truc comme "Personne n'aime la douleur pour la douleur...".<br/>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas culpa est quasi voluptatum ducimus, consequuntur nesciunt repudiandae tempora a sunt labore eius sit expedita illum aspernatur eos, numquam eaque, alias.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus obcaecati dicta numquam accusantium necessitatibus culpa harum, porro dolorem ipsum aspernatur iure earum aperiam excepturi distinctio error consequuntur veniam, neque facere!<br/> Ha et oui, Hufflepuff Forever ! ^^</p>
+  		<i><img id="hufflepuff" src="../img/Hufflepuff_Logo.png"><br/>Hufflepuff</i>
   		<span id="signature">Zandies Scepoli</span>
   	</div>
   </article>
 
-  <div class="comments">
-  	
+  <article>
+  	<div id="comments">
+  	<p id="title-comment">Commentaires</p>
+  	<div id="comment1">
+  		<p>Laocoon Donburi</p>
+  		<p>Date: 16 février 2018</p>
+  		<p>Heure: 15:40</p>
+  		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores ipsam corporis a possimus voluptates, quod aut, eos illum dolor impedit iure doloremque natus dolorem? Vitae incidunt rerum temporibus consectetur optio.</p>
+  	</div>
   </div>
-
-  <footer>
-    <a href="#"><img src="../img/Github_Logo.png" alt=""></a>
-    <p>
-      Fait avec les moyens du bord, à l'arrache comme on l'aime, l'image est gratuite et libre de droits - Zandies &copy; - Ca ne sert à rien mais c'est toujours classe d'avoir ce logo ;)
-    </p>
-    <a href="#"><img src="../img/Facebook_Logo.png" alt=""></a>
-  </footer>
-
+  </article>
   <script src="../js/test.js"></script>
 	</body>
 </html>
