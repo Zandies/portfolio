01d43bf73c7f812c1c5ccbc058763a6020a7bc9c
Zandies Scepoli <zandies.2017@gmail.com>
2018-02-16 15:29:27 +0100

    Add article in file's blog

--- php/blog.php
+++ php/blog.php
@@ -49,12 +49,31 @@
  
   <article>
   	<div class="banner-article">
-  		<p id="title">Lorem ipsum</p>
+  		<p id="banner-title">Lorem ipsum</p>
   	</div>
   	<div class="content-article">
-  		<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident nobis nostrum nam, error et labore. Atque, molestiae.</h2>
+	  	<!-- <div class="info-article">
+	  		<img id="author" src="../img/author.jpg">
+	  	</div> -->
+  		<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident nobis nostrum nam, error et labore. Atque, molestiae.</h3>
+  		<p>Salut !  Ceci est mon premier article "intitulé "Lorem ipsum", un nom bien étrange qui est utilisé dans le domaine de l'informatique pour remplir des zones qui contiendront du texte mais on ne sait pas encore lequel.
+  		Donc voici le texte : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, aspernatur. Cupiditate dolor quae debitis, sequi saepe sit deserunt nobis porro, magnam, placeat omnis, reprehenderit vel quam voluptates soluta fuga magni." Cela signifie un truc comme "Personne n'aime la douleur pour la douleur...". Ha et oui, Hufflepuff Forever ! ^^</p>
+  		<img id="hufflepuff" src="../img/Hufflepuff_Logo.png">
+  		<span id="signature">Zandies Scepoli</span>
   	</div>
   </article>
+
+  <div class="comments">
+  	
+  </div>
+
+  <footer>
+    <a href="#"><img src="../img/Github_Logo.png" alt=""></a>
+    <p>
+      Fait avec les moyens du bord, à l'arrache comme on l'aime, l'image est gratuite et libre de droits - Zandies &copy; - Ca ne sert à rien mais c'est toujours classe d'avoir ce logo ;)
+    </p>
+    <a href="#"><img src="../img/Facebook_Logo.png" alt=""></a>
+  </footer>
 
   <script src="../js/test.js"></script>
 	</body>
